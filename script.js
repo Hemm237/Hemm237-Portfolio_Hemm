b787fc0a4f6c71aa1faed0e0da68ca302702fb74
hem narendra <hemnarendrapattem@gmail.com>
2024-06-08 17:11:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".auto-type",{
+    strings:["Civil Engineer", "Web Designer", "Youtuber",
+            "Editor","Writer","Dancer","Artist"],
+    typeSpeed:100,
+    backSpeed:100,
+    backYard:1000,
+    loop: true
+
+
+
+});
